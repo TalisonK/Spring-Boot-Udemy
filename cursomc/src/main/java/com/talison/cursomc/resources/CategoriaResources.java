2af8e208742bbab2e1a32e39dc076adcaf50a5ce
talison <talisonkennedy1@gmail.com>
2021-01-04 12:56:34 -0300

    Adição do REST e testes

--- cursomc/src/main/java/com/talison/cursomc/resources/CategoriaResources.java
+++ cursomc/src/main/java/com/talison/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,40 @@
 package com.talison.cursomc.resources;
 
+import com.talison.cursomc.domain.Categoria;
+import com.talison.cursomc.services.CategoriaServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
+    @Autowired
+    private CategoriaServices services;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = services.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ cursomc/src/main/java/com/talison/cursomc/services/CategoriaServices.java
@@ -0,0 +1,20 @@
+package com.talison.cursomc.services;
+
+import com.talison.cursomc.domain.Categoria;
+import com.talison.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaServices {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
